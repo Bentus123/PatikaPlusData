7dc1a7eae4fab78f5fd38b5008d22b1a028898fa
Arda Dinkçioğlu <98737545+Bentus123@users.noreply.github.com>
2024-02-19 16:59:56 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,41 @@
-package Collection;
-
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        //Değişkenler
+        int Matematik, Fizik, Kimya, Turkce, Tarih, Muzik;
 
-	public static void main(String[] args) {
-		System.out.print("3 adet sayı giriniz");
-		int a,b,c;
-		Scanner input = new Scanner(System.in);
-		a= input.nextInt();
-		b= input.nextInt();
-		c=input.nextInt();
-		System.out.println("Sonuç:" +(((a+b)*c)-b));
-		
-		
-	}
+        //input icin scanner sınıfı
+        Scanner input = new Scanner(System.in);
 
+        //Kullanıcı veri girisi
+        System.out.println("Matematik notunuzu giriniz : ");
+        Matematik = input.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz : ");
+        Fizik = input.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz : ");
+        Kimya = input.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz : ");
+        Turkce = input.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz : ");
+        Tarih = input.nextInt();
+
+        System.out.println("Müzik notunuzu giriniz : ");
+        Muzik = input.nextInt();
+
+        //Ortalama Degisken Olusumu
+        int ortalama = (Matematik + Fizik + Kimya + Turkce + Tarih + Muzik) / 6;
+
+        //Sonuc Yazdırma
+        if (ortalama < 60) {
+            System.out.println("Ortalama Değeriniz : " + ortalama + " Sınıfta Kaldı");
+        } else {
+            System.out.println("Ortalama Değeriniz : " + ortalama + " Sınıfı Geçti");
+        }
+
+    }
 }
